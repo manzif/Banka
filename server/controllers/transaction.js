a45ab81c00b7136269eb89482b3a58ad849ece53
Manzi Fabrice <manzif60@gmail.com>
2019-04-12 23:02:08 +0200

    credit an account

--- server/controllers/transaction.js
+++ server/controllers/transaction.js
@@ -70,7 +70,50 @@ class Transaction{
         
         res.status(201).json({ status: 201, data: data });
     }
+    credit(req,res){
+        const trans = {
+            accountNumber: req.params.account_number,
+            cashier: req.body.cashier,
+            amount: req.body.amount
+        };
+        const result = Validate.validateTransaction(trans);
+        if(result.error){
+            return res.status(400).json({ status: 400, error: result.error.details[0].message });
+        }
 
+        const findAccount = accounts.find(c => c.accountNumber === parseInt(req.params.account_number));
+        const findCashier = users.find(c => c.id === req.body.cashier && c.Type === 'Cashier');
+        if (!findAccount){
+            return res.status(400).json({ status: 400, error: 'Account not found' });
+        }
+        if (!findCashier){
+            return res.status(400).json({ status: 400, error: 'Cashier not found' });
+        }
+        if(findAccount.balance < trans.amount){
+            return res.status(400).json({ status: 400, error: 'You don\'t have that certain amount of money'});
+        }
+        const transaction = {
+            id: transactions.length + 1,
+            createdOn: new Date(),
+            accountNumber: parseInt(req.params.account_number),
+            cashier: req.body.cashier,
+            amount: req.body.amount,
+            oldBalance: findAccount.balance,
+            newBalance: findAccount.balance - req.body.amount
+        }
+        transactions.push(transaction);
+
+        const data = {
+            transactionId: transaction.id,
+            accountNumber: transaction.accountNumber,
+            amount: transaction.amount,
+            cashier: transaction.cashier,
+            transactionType: 'credit',
+            accountBalance: transaction.newBalance
+        }
+        
+        res.status(201).json({ status: 201, data: data });
+    }
 
     
 }
