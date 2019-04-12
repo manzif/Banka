9457624d017897adb6095cf8886c285ca7570065
Manzi Fabrice <manzif60@gmail.com>
2019-04-12 22:43:10 +0200

    delete account

--- server/routes/account.js
+++ server/routes/account.js
@@ -7,5 +7,6 @@ router.get('/api/v1/accounts', Account.getAll);
 router.post('/api/v1/accounts', Account.create);
 router.patch('/api/v1/accounts/:account_number/activate', Account.activate);
 router.patch('/api/v1/accounts/:account_number/deactivate', Account.deactivate);
+router.delete('/api/v1/accounts/:account_number', Account.delete);
 
 export default router;
