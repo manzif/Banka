3387969f4ba61a6646f6c4de9276e4e6c6f5213a
Manzi Fabrice <manzif60@gmail.com>
2019-04-18 14:55:40 +0200

    feat(made changes on all controllers, routes and helpers)

--- server/controllers/transaction.js
+++ server/controllers/transaction.js
@@ -2,11 +2,9 @@ import Validate from '../helpers/validate';
 import Arrays from '../arrays/arrays';
 
 
-
-
 class Transaction{
 
-    getAll(req,res){     
+    getAllTransaction(req,res){     
         res.status(200).json({ status: 200, data: Arrays.transactions });
     }
 
@@ -20,27 +18,29 @@ class Transaction{
         if(result.error){
             return res.status(400).json({ status: 400, error: result.error.details[0].message });
         }
-       const cashiers =[{cashier:1, names: 'Felix' }, {cashier:2, names: 'Niyo' }];
-
-        const findAccount = Arrays.accounts.find(c => c.accountNumber === parseInt(req.params.account_number));
-        const findCashier = cashiers.find(c => c.cashier === req.body.cashier);
-        console.log(findCashier)
+        const cashiers =[{cashier:1, names: 'Felix' }, {cashier:2, names: 'Niyo' }];
+        const findAccount = Arrays.accounts.find(user => user.accountNumber === parseInt(req.params.account_number));
+        const findCashier = cashiers.find(user => user.cashier === req.body.cashier);
         if (!findAccount){
-            return res.status(400).json({ status: 400, error: 'Account not found' });
+            return res.status(400).json({ status: 404, error: 'Account not found' });
         }
         if (!findCashier){
-            return res.status(400).json({ status: 400, error: 'Cashier not found' });
+            return res.status(400).json({ status: 404, error: 'Cashier not found' });
+        }
+        if(findAccount.balance < trans.amount){
+            return res.status(400).json({ status: 400, error: 'You don\'t have that certain amount of money'});
         }
         const transaction = {
-            id: transactions.length + 1,
+            id: Arrays.transactions.length + 1,
             createdOn: new Date(),
             accountNumber: parseInt(req.params.account_number),
             cashier: req.body.cashier,
             amount: req.body.amount,
             oldBalance: findAccount.balance,
-            newBalance: findAccount.balance + req.body.amount
+            newBalance: findAccount.balance - req.body.amount
         }
-        transactions.push(transaction);
+        Arrays.transactions.push(transaction);
+        findAccount.balance = transaction.newBalance;
 
         const data = {
             transactionId: transaction.id,
@@ -63,28 +63,27 @@ class Transaction{
         if(result.error){
             return res.status(400).json({ status: 400, error: result.error.details[0].message });
         }
-
-        const findAccount = accounts.find(c => c.accountNumber === parseInt(req.params.account_number));
-        const findCashier = users.find(c => c.id === req.body.cashier && c.Type === 'Cashier');
+        const cashiers =[{cashier:1, names: 'Felix' }, {cashier:2, names: 'Niyo' }];
+        const findAccount = Arrays.accounts.find(c => c.accountNumber === parseInt(req.params.account_number));
+        const findCashier = cashiers.find(c => c.cashier === req.body.cashier);
         if (!findAccount){
             return res.status(400).json({ status: 400, error: 'Account not found' });
         }
         if (!findCashier){
             return res.status(400).json({ status: 400, error: 'Cashier not found' });
         }
-        if(findAccount.balance < trans.amount){
-            return res.status(400).json({ status: 400, error: 'You don\'t have that certain amount of money'});
-        }
+        
         const transaction = {
-            id: transactions.length + 1,
+            id: Arrays.transactions.length + 1,
             createdOn: new Date(),
             accountNumber: parseInt(req.params.account_number),
             cashier: req.body.cashier,
             amount: req.body.amount,
             oldBalance: findAccount.balance,
-            newBalance: findAccount.balance - req.body.amount
+            newBalance: findAccount.balance + req.body.amount
         }
-        transactions.push(transaction);
+        Arrays.transactions.push(transaction);
+        findAccount.balance = transaction.newBalance;
 
         const data = {
             transactionId: transaction.id,
@@ -96,9 +95,7 @@ class Transaction{
         }
         
         res.status(201).json({ status: 201, data: data });
-    }
-
-    
+    }    
 }
 
 export default new Transaction();
